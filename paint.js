a5db2ae9787cdd2fc87af6926d5adb53cd61dc07
unknown <tonylehtonen_750@hotmail.com>
2023-10-03 21:35:30 +0300

    suht valmis

--- paint.js
+++ paint.js
@@ -11,6 +11,10 @@ function myohaistaAlert(){
 kärpäsen kunnes niitä on vaikka 100*/
 function teeKarpasia(){
 	
+	function katoamisenViive(){
+	karpanenKuva.parentElement.remove();
+}
+	
 	setTimeout(teeKarpasia, 300);
 	karpaset++;
 	if (karpaset < 100){
@@ -27,7 +31,11 @@ function teeKarpasia(){
 	karpanenKuva.addEventListener("click", function kuollutKarpanen(){
 		karpanenKuva.setAttribute("class", "kuollutKarpanen");
 		karpanenKuva.setAttribute("src", "kuvat/karpanen_liiskattu-removebg-preview.png");
-	
+		karpanenKuva.parentElement.setAttribute("position", "absolute");
+		karpanenKuva.removeAttribute("click", "function kuollutKarpanen()");
+		
+		setTimeout(katoamisenViive, 2000);
+			
 		kuolleetKarpasetCounter++;
 		document.getElementById("kuolleidenMaara").innerHTML = kuolleetKarpasetCounter;
 	});
@@ -38,33 +46,3 @@ function teeKarpasia(){
 	}
 }
 
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("kuva");
-  let dots = document.getElementsByClassName("pallo");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
